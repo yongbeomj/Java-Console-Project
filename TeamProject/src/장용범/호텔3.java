36f7c1460403e725d751c3ef95f450542b019383
JYB <alex.ybjang@gmail.com>
2021-10-22 11:44:16 +0900

    호텔예약 인터페이스 및 하위 클래스 생성

--- TeamProject/src/장용범/호텔3.java
+++ TeamProject/src/장용범/호텔3.java
@@ -2,4 +2,30 @@ package 장용범;
 
 public class 호텔3 extends 호텔Total {
 
+	@Override
+	public void 예약등록() {
+		// TODO Auto-generated method stub
+		super.예약등록();
+	}
+	@Override
+	public void 예약취소() {
+		// TODO Auto-generated method stub
+		super.예약취소();
+	}
+	@Override
+	public void 본인예약조회() {
+		// TODO Auto-generated method stub
+		super.본인예약조회();
+	}
+	@Override
+	public void 전체예약현황() {
+		// TODO Auto-generated method stub
+		super.전체예약현황();
+	}
+	@Override
+	public void 파일처리() {
+		// TODO Auto-generated method stub
+		super.파일처리();
+	}
+	
 }
