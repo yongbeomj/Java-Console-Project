643f978047b30bdde57709194d27b6baead8d0d9
swh7j <91528977+swh7j@users.noreply.github.com>
2021-10-25 14:14:47 +0900

    Add files via upload

--- TeamProject/src/신우현/호텔1.java
+++ TeamProject/src/신우현/호텔1.java
@@ -1,32 +1,68 @@
-package 팀프로젝트;
+package 신우현;
+
+import Reference.Main;
+import Reference.Ticket;
 
 public class 호텔1 extends 호텔Total {
-	
-	final String 예약코드 = "01";
 
+	// 메소드
 	@Override
-	public void 예약등록() {
-		// TODO Auto-generated method stub
-		super.예약등록();
+	public void 예약등록(int hotelch, int roomch) {
+		if (roomch == 1) {
+			if (room[hotelch-1][roomch-1] == 0) {
+				System.out.println("호텔1 / 스위트룸 예약 완료");
+				room[hotelch-1][roomch-1] = 1;
+			}
+		} else if (roomch == 2) {
+			if (room[hotelch-1][roomch-1] == 0) {
+				System.out.println("호텔1 / 디럭스룸 예약 완료");
+				room[hotelch-1][roomch-1] = 1;
+			}
+		} else if (roomch == 3) {
+			if (room[hotelch-1][roomch-1] == 0) {
+				System.out.println("호텔1 / 스탠다드룸 예약 완료");
+				room[hotelch-1][roomch-1] = 1;
+			}
+		}
 	}
 	@Override
-	public void 예약취소() {
-		// TODO Auto-generated method stub
-		super.예약취소();
+	public void 예약취소(int hotelch, int roomch) {
+		if (roomch == 1) {
+			if (room[hotelch-1][roomch-1] == 1) {
+				System.out.println("호텔1 / 스위트룸 예약취소 완료");
+				room[hotelch-1][roomch-1] = 0;
+			}
+		} else if (roomch == 2) {
+			if (room[hotelch-1][roomch-1] == 1) {
+				System.out.println("호텔1 / 디럭스룸 예약취소 완료");
+				room[hotelch-1][roomch-1] = 0;
+			}
+		} else if (roomch == 3) {
+			if (room[hotelch-1][roomch-1] == 1) {
+				System.out.println("호텔1 / 스탠다드룸 예약취소 완료");
+				room[hotelch-1][roomch-1] = 0;
+			}
+		}
 	}
 	@Override
 	public void 본인예약조회() {
-		// TODO Auto-generated method stub
-		super.본인예약조회();
+		System.out.println("호텔 1 본인예약조회");
 	}
 	@Override
 	public void 전체예약현황() {
-		// TODO Auto-generated method stub
-		super.전체예약현황();
+		System.out.print("호텔 1\t");
+		for (int i = 0; i < 3; i++) {
+			if (room[0][i] == 0)
+				System.out.print("□\t");
+			if (room[0][i] == 1)
+				System.out.print("■\t");
+		}
+		System.out.println();
 	}
 	@Override
 	public void 파일처리() {
-		// TODO Auto-generated method stub
-		super.파일처리();
+		System.out.println("호텔 1 파일처리");
 	}
+	
+	
 }
--- TeamProject/src/신우현/호텔3.java
+++ TeamProject/src/신우현/호텔3.java
@@ -1,33 +1,63 @@
-package 팀프로젝트;
+package 신우현;
 
 public class 호텔3 extends 호텔Total {
-	
-	final String 예약코드 = "03";
 
 	@Override
-	public void 예약등록() {
-		// TODO Auto-generated method stub
-		super.예약등록();
+	public void 예약등록(int hotelch, int roomch) {
+		if (roomch == 1) {
+			if (room[hotelch-1][roomch-1] == 0) {
+				System.out.println("호텔3 / 스위트룸 예약 완료");
+				room[hotelch-1][roomch-1] = 1;
+			}
+		} else if (roomch == 2) {
+			if (room[hotelch-1][roomch-1] == 0) {
+				System.out.println("호텔3 / 디럭스룸 예약 완료");
+				room[hotelch-1][roomch-1] = 1;
+			}
+		} else if (roomch == 3) {
+			if (room[hotelch-1][roomch-1] == 0) {
+				System.out.println("호텔3 / 스탠다드룸 예약 완료");
+				room[hotelch-1][roomch-1] = 1;
+			}
+		}
 	}
 	@Override
-	public void 예약취소() {
-		// TODO Auto-generated method stub
-		super.예약취소();
+	public void 예약취소(int hotelch, int roomch) {
+		if (roomch == 1) {
+			if (room[hotelch-1][roomch-1] == 1) {
+				System.out.println("호텔3 / 스위트룸 예약취소 완료");
+				room[hotelch-1][roomch-1] = 0;
+			}
+		} else if (roomch == 2) {
+			if (room[hotelch-1][roomch-1] == 1) {
+				System.out.println("호텔3 / 디럭스룸 예약취소 완료");
+				room[hotelch-1][roomch-1] = 0;
+			}
+		} else if (roomch == 3) {
+			if (room[hotelch-1][roomch-1] == 1) {
+				System.out.println("호텔3 / 스탠다드룸 예약취소 완료");
+				room[hotelch-1][roomch-1] = 0;
+			}
+		}
 	}
 	@Override
 	public void 본인예약조회() {
-		// TODO Auto-generated method stub
-		super.본인예약조회();
+		System.out.println("호텔 3 본인예약조회");
 	}
 	@Override
 	public void 전체예약현황() {
-		// TODO Auto-generated method stub
-		super.전체예약현황();
+		System.out.print("호텔 3\t");
+		for (int i = 0; i < 3; i++) {
+			if (room[2][i] == 0)
+				System.out.print("□\t");
+			if (room[2][i] == 1)
+				System.out.print("■\t");
+		}
+		System.out.println();
 	}
 	@Override
 	public void 파일처리() {
-		// TODO Auto-generated method stub
-		super.파일처리();
+		System.out.println("호텔 3 파일처리");
 	}
 	
 }
