f1fd9c037e18f3ffb71b192664f93ebd5aad30ce
swh7j <91528977+swh7j@users.noreply.github.com>
2021-10-27 14:34:06 +0900

    Add files via upload

--- TeamProject/src/신우현/TestMain.java
+++ TeamProject/src/신우현/TestMain.java
@@ -27,7 +27,7 @@ public class TestMain {
 				System.out.println("\t1.회원가입\t2.로그인\t\t3.종료");
 				System.out.println(" ============================================================= ");
 				System.out.print(">>> 선택 : ");
-				
+
 				int ch = scanner.nextInt();
 				if (ch == 1) { // 회원가입
 
@@ -43,13 +43,14 @@ public class TestMain {
 
 					if (ch1 == 1) { // 회원
 						System.out.println(" ============================================================= ");
-						System.out.print(" Id : ");	String h_id = scanner.next();
-						System.out.print(" Password : "); String h_pw = scanner.next();
+						System.out.print(" Id : ");
+						String h_id = scanner.next();
+						System.out.print(" Password : ");
+						String h_pw = scanner.next();
 
 						boolean result = MemberController.login(h_id, h_pw);
 						if (result) {// 로그인 성공시
 							System.err.println(" [알림] : 로그인 성공 ");
-
 
 							while (true) {
 								System.out.println(" ============================================================= ");
@@ -59,15 +60,16 @@ public class TestMain {
 								int ch2 = scanner.nextInt();
 
 								if (ch2 == 1) { // 예약등록
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println(">>호텔지점 선택<<");
 									System.out.println("1.신라호텔 2.라마다호텔 3.그랜드호텔");
 									int hotelch = TestMain.scanner.nextInt();
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println(">>방 타입 선택<<");
 									System.out.println("1.스위트 2.디럭스 3.스탠다드");
 									int roomch = TestMain.scanner.nextInt();
-									
 
 									if (hotelch == 1) {
 										호텔예약 = new 호텔1();
@@ -95,11 +97,13 @@ public class TestMain {
 									}
 
 								} else if (ch2 == 2) { // 예약취소
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println(">>호텔지점 선택<<");
 									System.out.println("1.신라호텔 2.라마다호텔 3.그랜드호텔");
 									int hotelch = TestMain.scanner.nextInt();
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println(">>방 타입 선택<<");
 									System.out.println("1.스위트 2.디럭스 3.스탠다드");
 									int roomch = TestMain.scanner.nextInt();
@@ -129,31 +133,39 @@ public class TestMain {
 									}
 
 								} else if (ch2 == 3) { // 본인예약조회
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println("\t\t\t본인예약현황조회");
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									호텔예약 = new 호텔1();
 									호텔예약.본인예약조회(h_id);
 									호텔예약 = new 호텔2();
 									호텔예약.본인예약조회(h_id);
 									호텔예약 = new 호텔3();
 									호텔예약.본인예약조회(h_id);
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 								} else if (ch2 == 4) { // 전체예약현황조회
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println("\t\t\t전체예약현황조회");
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println("호텔\t\t스위트룸\t\t디럭스룸\t\t스탠다드룸");
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									호텔예약 = new 호텔1();
 									호텔예약.전체예약현황_회원();
 									호텔예약 = new 호텔2();
 									호텔예약.전체예약현황_회원();
 									호텔예약 = new 호텔3();
 									호텔예약.전체예약현황_회원();
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 								} else if (ch2 == 5) { // 방정보
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println(" 방정보 : 1.스탠다드 2.디럭스 3.스위트 ");
 									System.out.println(">>> 선택 : ");
 									int ch3 = scanner.nextInt();
@@ -186,10 +198,9 @@ public class TestMain {
 						System.out.print(" Id : ");
 						String h_id = scanner.next();
 						System.out.print(" Password : ");
-		
 						String h_pw = scanner.next();
-						System.out.println(" ============================================================= ");
-						if (h_id.equals("admin")) {
+
+						if (h_id.equals("admin") && h_pw.equals("1234")) {
 							System.err.println(" 관리자가 로그인하셨습니다. ");
 							while (true) {
 								System.out.println(" ============================================================= ");
@@ -198,29 +209,35 @@ public class TestMain {
 								System.out.print(">>> 선택 : ");
 								int ch3 = scanner.nextInt();
 								if (ch3 == 1) {
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println("\t\t\t전체예약현황조회");
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									System.out.println("호텔\t\t스위트룸\t\t디럭스룸\t\t스탠다드룸");
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									호텔예약 = new 호텔1();
 									호텔예약.전체예약현황_관리자();
 									호텔예약 = new 호텔2();
 									호텔예약.전체예약현황_관리자();
 									호텔예약 = new 호텔3();
 									호텔예약.전체예약현황_관리자();
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 
 								} else if (ch3 == 2) {
 									System.out.println(" ========================== 회원 정보 ========================== ");
 									System.out.println("회원ID\t   이름\t       연락처");
-									System.out.println(" ============================================================= ");
+									System.out
+											.println(" ============================================================= ");
 									Member member = new Member(h_id, h_pw, h_id, h_pw);
 									for (Member temp : MemberController.memberlist) {
 										if (!temp.getH_id().equals("admin")) {
 											System.out.println("ID : " + temp.getH_id() + "/ 이름 : " + temp.getH_name()
 													+ "/ 연락처 : " + temp.getH_phone());
-											System.out.println(" ============================================================= ");
+											System.out.println(
+													" ============================================================= ");
 
 										}
 									}
@@ -233,6 +250,8 @@ public class TestMain {
 								}
 							}
 
+						} else {
+							System.err.println(" 회원정보가 다릅니다. ");
 						}
 
 					} else {
